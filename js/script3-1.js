0b5ea34a185617ffcaee14ee6cfb2d0a370703ec
Kari Eding <wndjnky1@gmail.com>
2016-06-29 10:48:54 -0700

    Add assignment03-1.html, assignment03-2.html,js/script3-1.js,js/script3-2.js to Assignment03 repo

---
+++ js/script3-1.js
@@ -0,0 +1,261 @@
+//
+// COMM644:Assignment 3 - Part 1
+//
+
+/*
+// STEP 1
+var favMovies = ['Dirty Dancing', 'The Matrix', 'The Avengers', 'Casablanca', 'Point Break']; 	
+ console.log(favMovies[1]);
+*/
+
+/*
+// STEP 2
+var movies = [];
+    movies[0]='Dirty Dancing';
+    movies[1]='The Matrix';
+    movies[2]='The Avengers';
+    movies[3]='Casablanca';
+    movies[4]='Point Break';
+console.log(movies[0]);
+*/
+
+/*
+//
+// STEP 3
+//
+var movies = ['Dirty Dancing', 'The Matrix', 'The Avengers', 'Casablanca', 'Point Break'];
+    movies.splice(2,0, 'Sound of Music');
+console.log(movies.length);
+*/
+
+
+/*
+//
+// STEP 4
+var movies = [];
+    movies[0]='Dirty Dancing';
+    movies[1]='The Matrix';
+    movies[2]='The Avengers';
+    movies[3]='Casablanca';
+    movies[4]='Point Break';
+    //or movies.shift();
+    delete movies[0];
+    console.log(movies);
+*/
+
+/*
+// STEP 5
+var movies = [];
+    movies[0]='Dirty Dancing'; 
+    movies[1]='The Matrix';
+    movies[2]='The Avengers';
+    movies[3]='Casablanca';
+    movies[4]='Hunger Games';
+    movies[5]='Point Break';
+    movies[6]='Star Wars';
+    for(i=0; i < 7; i++) {
+    console.log(movies[i]);
+     }
+*/
+
+/*
+ // STEP 6
+var movies = ['Dirty Dancing','The Matrix','The Avengers','Casablanca', 'Hunger Games', 'Point Break', 'Star Wars'];
+    for (var x in movies) {
+        console.log(movies[x]);
+    }
+//
+*/
+
+/*
+// STEP 7
+var movies = ['Dirty Dancing','The Matrix','The Avengers','Casablanca', 'Hunger Games', 'Point Break', 'Talladega Nights'];
+    for (var x in movies.sort()) {
+    console.log(movies[x]);
+    }
+*/
+
+/*
+// STEP 8
+var movies = ['Dirty Dancing','The Matrix','The Avengers','Casablanca', 'Hunger Games', 'Point Break', 'Talladega Nights'];
+var leastFavMovies = ['Pulp Fiction','Rocky IV', 'Bring It On'];
+
+console.log('Movies I like:' + "\n"+"\n") 
+for (i=0; i < movies.length; i++) {
+    console.log(movies[i] +"\n" );
+}
+console.log('\n' + '\n' + 'Movies I regret watching:' + '\n' + '\n');
+for  (j=0; j < leastFavMovies.length; j++) {
+    console.log(leastFavMovies[j] +"\n");
+}
+*/
+
+/*
+// STEP 9
+var movies = ['Dirty Dancing','The Matrix','The Avengers','Casablanca', 'Hunger Games', 'Point Break', 'Talladega Nights'];
+var leastFavMovies = ['Pulp Fiction','Rocky IV', 'Bring It On'];
+    movies = movies.concat(leastFavMovies);
+    movies = (movies.sort());
+    console.log(movies.reverse());
+*/
+
+/*
+// STEP 10
+var movies = ['Dirty Dancing','The Matrix','The Avengers','Casablanca', 'Hunger Games', 'Point Break', 'Talladega Nights'];
+var leastFavMovies = ['Pulp Fiction','Rocky IV', 'Bring It On'];
+    movies = movies.concat(leastFavMovies);
+    movies = (movies.sort());
+    movies = (movies.reverse());
+    console.log(movies.pop());
+*/
+
+/*
+// STEP 11
+var movies = ['Dirty Dancing','The Matrix','The Avengers','Casablanca', 'Hunger Games', 'Point Break', 'Talladega Nights'];
+var leastFavMovies = ['Pulp Fiction','Rocky IV', 'Bring It On'];
+    movies = movies.concat(leastFavMovies);
+    movies = (movies.sort());
+    movies = (movies.reverse());
+    console.log(movies.shift());
+*/
+
+/*
+// STEP 12
+var movies = ['Dirty Dancing','The Matrix','The Avengers','Casablanca', 'Hunger Games', 'Point Break', 'Talladega Nights'];
+var leastFavMovies = ['Pulp Fiction','Rocky IV', 'Bring It On'];
+    movies = movies.concat(leastFavMovies);
+    movies = (movies.sort());
+    movies = (movies.reverse());
+var moviesNoLike = movies.indexOf('Rocky IV') + '\n' + movies.indexOf('Pulp Fiction') + '\n' + movies.indexOf('Bring It On') ;
+    console.log(moviesNoLike);
+var allGoodMovies = movies.splice(3,2,'Edward Scissorhands', 'Monty Python') + '\n' + movies.splice(9,1,'Young Frankenstein');
+    console.log(movies);
+*/
+
+/*
+// STEP 13
+var employee1 = [];
+    employee1['employee id'] = '1';
+    employee1['name'] = 'Betty Jones';        
+    employee1['title'] = 'Sr. Consultant';
+    employee1['department'] = 'Software Development'; 
+    employee1['currently employed'] = 'yes';
+
+var employee2 = [];
+    employee2['employee id'] = '2';
+    employee2['name'] = 'Dave Bradley';        
+    employee2['title'] = 'Programmer';
+    employee2['department'] = 'Development'; 
+    employee2['currently employed'] = 'yes';
+
+var employees = [employee1,employee2];
+    console.log(employees[1]['name']);
+*/
+
+/*
+//// STEP 14
+var employee1 = [];
+    employee1['employee id'] = '1';
+    employee1['name'] = 'Betty Jones';        
+    employee1['title'] = 'Sr. Consultant';
+    employee1['department'] = 'Software Development'; 
+    employee1['currently employed'] = 'yes';
+
+var employee2 = [];
+    employee2['employee id'] = '2';
+    employee2['name'] = 'Dave Bradley';        
+    employee2['title'] = 'Programmer';
+    employee2['department'] = 'Development'; 
+    employee2['currently employed'] = 'yes';
+
+var employees = [employee1,employee2];
+for (var i=0; i<employees.length; i++){
+    console.log(employees[i]["name"]);              
+ }
+*/
+
+/*
+// STEP 15
+var employee1 = [];
+    employee1['employee id'] = '1';
+    employee1['name'] = 'Betty Jones';        
+    employee1['title'] = 'Sr. Consultant';
+    employee1['department'] = 'Software Development'; 
+    employee1['currently employed'] = 'yes';
+
+var employee2 = [];
+    employee2['employee id'] = '2';
+    employee2['name'] = 'Dave Bradley';        
+    employee2['title'] = 'Programmer';
+    employee2['department'] = 'Development'; 
+    employee2['currently employed'] = 'yes';
+
+var employee3 = [];
+    employee3['employee id'] = '3';
+    employee3['name'] = 'Chris Porter';        
+    employee3['title'] = 'IT Manager';
+    employee3['department'] = 'IT'; 
+    employee3['currently employed'] = 'no';
+
+var employees = [employee1,employee2,employee3];
+
+console.log('Currently employed with the company: ' +'\n' + '\n');
+for (var i=0; i<employees.length; i++){
+    if(employees[i]['currently employed'] == 'yes') {
+    console.log(employees[i]['name'] + '\n'); 
+        }
+    else if
+    (employees[i]['currently employed'] == 'no') {
+        console.log('\n' + 'Not currently employed with the company: ' +'\n' + '\n');
+        console.log(employees[i]['name'] + '\n');
+    }
+ }
+*/ 
+ 
+/*
+// STEP 16
+
+var movies = [['Dirty Dancing', 1],['The Matrix', 2],['Monty Python', 3],['Point Break', 4],['Rocky Horror Picture Show', 5]];
+var i, j;
+for (i in movies) {
+    for (j in movies[i]) {
+var myTitles = movies[i].filter(function(item) {    
+    return typeof item === "string";
+    });
+  console.log(myTitles[0]);  
+    }
+}
+*/
+
+
+// PRACTICE WITH FUNCTIONS
+/*   
+// STEP 1
+//Anonymous function displayMessage()
+var displayMessage = function(param) {
+    console.log(param); 
+}
+displayMessage('This is a message just for you'); 
+*/
+
+/*
+//
+// STEP 2
+// Named function calculate
+function calculate(x,y) {
+  var remainder = (x % y);
+        window.console.log('The remainder is: ' + remainder);       
+    } 
+calculate(25, 12);
+*/
+ 
+/*
+// STEP 3
+var employees = ['Zak', 'Jessica', 'Mark', 'Fred', 'Sally'];
+console.log('Employees:' + '\n' + '\n');
+for(var i = 0; i <employees.length; i++) {
+    var showEmployee = function(employees){
+    }
+    console.log(employees[i] +'\n' );
+}
+*/
---
+++ js/script3-2.js
@@ -0,0 +1,208 @@
+//
+// COMM644:Assignment 3 - Part 2
+//
+
+/*
+//// ROCK, PAPER, SCISSORS GAME
+// 	
+// STEP 1
+//Rock destroys scissors.
+//Scissors cut paper.
+//Paper covers rock.
+//
+var compChoice = Math.random();
+var yourChoice = prompt('We are playing Rock, Paper, Scissors.' + '\n' + 'Choose one of the following: (rock, paper, or scissors)  ');
+if(yourChoice !== ('rock'|| 'paper' || 'scissors' )){
+    window.alert('You entered an incorrect value. Please try again!');
+}
+
+if(compChoice < .34) {
+    compChoice = 'rock';
+} else if (compChoice <.67) {
+    compChoice = 'paper';
+} else {
+    compChoice = 'scissors';
+}
+window.console.log('Your opponent chose: ' + compChoice);
+//need to compare the two choices
+var result = function compare(pick1, pick2) {
+    if(pick1 === pick2) {
+        window.alert('It\'s a tie! Try again. ');
+    }
+    else if(pick1 === 'rock') {
+        if(pick2 === 'scissors'){
+            window.console.log('Rock destroys scissors.  You win! ');
+        } else {
+             window.alert('Paper covers rock. Sorry you lose! ');
+        }
+    }  
+    else if(pick1 === 'paper') {
+        if(pick2 === 'rock') {
+            window.alert('Paper covers rock. You win! ');
+        } else {
+             window.alert('Scissors cut paper. Sorry you lose! ');
+        }
+ 
+    }
+     else if(pick1 === 'scissors') {
+        if(pick2 === 'paper') {
+            window.alert('Scissors cut paper. You win! ');
+        } else {
+             window.alert('Rock destroys scissors.  Sorry you lose! ');
+        }
+ 
+    }
+   
+}
+ result(yourChoice, compChoice);
+*/
+
+/*
+// THE BASIC CALCULATOR
+//
+var yourNum1 = prompt('Enter the first number of the operation you wish to perform: ' );
+var yourNum2 = prompt('Enter the second number of the operation you wish to perform: ' );
+var yourOp = prompt('Enter the operation you wish to perform (add, subtract, multiply, divide: ');
+
+var convrt1 = Number(yourNum1);
+var convrt2 = Number(yourNum2);
+
+//var result;
+ function calculate(num1, num2, operation) {
+  switch ( yourOp ) {
+    case 'add':''
+        window.alert('The result of your calculation: ' + yourNum1 + ' + ' + yourNum2 + ' = ' + (convrt1 + convrt2));
+        break;
+    case 'subtract':
+         alert('The result of your calculation: ' + yourNum1 + ' - ' +  yourNum2 + ' = ' + (convrt1 - convrt2));
+        break;
+    case 'multiply':
+        alert('The result of your calculation: ' + yourNum1 + ' * ' +  yourNum2 + ' = ' + (convrt1 * convrt2));
+        break;
+    case 'divide':
+        alert('The result of your calculation: ' + yourNum1 + ' / ' +  yourNum2 + ' = ' + (convrt1 / convrt2));
+        break;
+    default:
+        alert('You have not entered add, subtract, multiply, or divide. Please enter all values if you wish to complete the calculation.');
+        
+        break;
+  }  
+    
+}
+calculate(convrt1, convrt2, yourOp);
+*/
+
+
+/*
+// DEATH BY JAVASCRIPT
+//
+//  STEP 1
+
+function sortMe(someString) {
+    var splitStr = (someString.split(''));
+         sortStr = splitStr.sort();
+return   sortStr.join('');  
+  }  
+console.log(sortMe('supercalifragilisticexpialidocious')); 
+//
+*/
+
+/*
+//
+//  STEP 2
+//
+window.console.log('The original string is: how now brown cow where goest thou');
+//
+function capMe(someString) {
+//   console.log(someString);
+//
+//   split string into array
+var splitStr = [someString.split(' ')];
+//    console.log(splitStr);
+//   
+// for each index location set value = (strip first letter & change to upper case) + (the remaining part of the word) 
+//  originally used substr instead of slice, but saw that substr has been deprecated. So updated to slice.
+ for (var x in splitStr) {
+ var myWords = splitStr[x];     
+};
+//   
+//  re-assemble string and display in console
+
+console.log(myWords[0].charAt(0).toUpperCase() + myWords[0].slice(1) + ' ' + 
+        myWords[1].charAt(0).toUpperCase() + myWords[1].slice(1) + ' ' + 
+        myWords[2].charAt(0).toUpperCase() + myWords[2].slice(1) + ' ' + 
+        myWords[3].charAt(0).toUpperCase() + myWords[3].slice(1) + ' ' +  
+        myWords[4].charAt(0).toUpperCase() + myWords[4].slice(1) + ' ' + 
+        myWords[5].charAt(0).toUpperCase() + myWords[5].slice(1) + ' ' +  
+        myWords[6].charAt(0).toUpperCase() + myWords[6].slice(1)); 
+        }
+// call function capMe
+capMe('how now brown cow where goest thou');
+//
+//
+*/
+
+/*
+//
+// STEP 3
+//
+// count your vowels
+//
+function howManyVowels(someString) {
+//
+//   search string for vowels and output to # of vowels found to the console
+//
+var result = someString.match(/[aeiou]/g);
+//     window.console.log(result);
+     window.console.log(result.length);
+//
+}
+// call function howManyVowels
+howManyVowels('how now brown cow where goest thou');
+//
+*/
+
+/*
+//
+// STEP 4
+//
+var randomStr = function() {
+    var availChar = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+    var lengthOfString = 8; // desired length of string
+    var randomStr = (''); // start with empty string
+    for (var i=0; i < lengthOfString; i++) {
+            var x = Math.random(); // generate random number
+            var y = availChar.length; // number of available choices
+            var z = Math.floor(x * y); // round product down to nearest integer
+            randomStr += availChar.charAt(z); // match integer to position in availChar & return
+  }
+    return randomStr;
+}
+console.log(randomStr(''))  ;  
+//
+//
+*/
+
+/*
+//
+// STEP 5
+// Built into "sort" function
+// Less than 0: "a" gets sorted to be a lower index than "b"
+// Zero: "a" and "b" are considered equal, and no sorting performed.
+// Greater than 0:  "b" gets sorted to be a lower index than "a". Then the next pair is assessed.
+// Longest value is then found in position[0]
+// var countryNames = ['country1', 'country2', 'country3', 'country4', 'country5'];
+
+var countryNames = ['Uruguay', ' Australia', 'New Zealand', 'Germany', 'The Netherlands'];
+    countryNames.sort(function(country1, country2) {
+//        console.log(country2);     
+//        console.log(country1);          
+//        console.log(country2.length - country1.length);
+//        
+        return (country2.length - country1.length);
+        });     
+             
+    console.log('The country with the longest name is :' + countryNames[0]);
+//
+//
+*/
